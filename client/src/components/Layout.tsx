8f55fbbdca94b4659748326ac13f297264fb32b2
Jeleel Kolapo Abidemi <kolly4weall@gmail.com>
2025-09-07 11:21:19 +0100

    roles enabled

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -124,7 +124,7 @@ const Layout = ({ children }: LayoutProps) => {
         { name: 'Fiscal Calendar', href: '/management/fiscal' },
         { name: 'Chart of Accounts', href: '/management/chart-of-accounts' },
         { name: 'Cash Accounts', href: '/management/cash-accounts' },
-        { name: 'Approval Flows', href: '/management/approvals' },
+        // { name: 'Approval Flows', href: '/management/approvals' },
         { name: 'Role Management', href: '/management/roles' },
         { name: 'User Management', href: '/management/users' }
       ]
