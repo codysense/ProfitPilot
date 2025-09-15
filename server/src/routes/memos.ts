c45234786b271d0927a133755e191118c330caff
Jeleel Kolapo Abidemi <kolly4weall@gmail.com>
2025-09-15 11:18:45 +0100

    route memos fix

--- server/src/routes/memos.ts
+++ server/src/routes/memos.ts
@@ -1,5 +1,29 @@
-import express from 'express';
-import memosController from '../controllers/memos';
+// import express from 'express';
+// import { createSalesMemo, getSalesMemos, createPurchaseMemo, getPurchaseMemos } from "../controllers/memos";
+
+// // import memosController from '../controllers/memos';
+// const router = express.Router();
+// router.use('/', memosController as any);
+// export default router;
+
+
+import express from "express";
+import {
+  createSalesMemo,
+  getSalesMemos,
+  createPurchaseMemo,
+  getPurchaseMemos,
+} from "../controllers/memos";
+
 const router = express.Router();
-router.use('/', memosController as any);
+
+// Sales memos
+router.post("/sales", createSalesMemo);
+router.get("/sales", getSalesMemos);
+
+// Purchase memos
+router.post("/purchase", createPurchaseMemo);
+router.get("/purchase", getPurchaseMemos);
+
 export default router;
+
