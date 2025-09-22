a60688d5b72494d50a635e55188ec24a11707e84
Jeleel Kolapo Abidemi <kolly4weall@gmail.com>
2025-09-22 12:31:56 +0100

    all items

--- client/src/pages/purchases/CreatePurchaseModal.tsx
+++ client/src/pages/purchases/CreatePurchaseModal.tsx
@@ -59,7 +59,7 @@ const CreatePurchaseModal = ({ onClose, onSuccess }: CreatePurchaseModalProps) =
 
   const { data: items } = useQuery({
     queryKey: ['items-for-purchases'],
-    queryFn: () => inventoryApi.getItems({ type:'FINISHED_GOODS', limit: 100 })
+    queryFn: () => inventoryApi.getItems({  limit: 100 })
   });
 
   const calculateTotal = () => {
