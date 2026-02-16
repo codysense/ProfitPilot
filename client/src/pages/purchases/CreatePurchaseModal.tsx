8bd6ea89f5abec84087d3f394c24e7980ad1afd5
Jeleel Kolapo <kolly4weall@gmail.com>
2026-02-16 17:00:49 +0100

    quantity value validation

--- client/src/pages/purchases/CreatePurchaseModal.tsx
+++ client/src/pages/purchases/CreatePurchaseModal.tsx
@@ -357,7 +357,7 @@ const CreatePurchaseModal = ({
                             })}
                             type="number"
                             // onScroll={}
-                            step="10.01"
+                            step="0.01"
                             className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                             placeholder="1.00"
                           />
