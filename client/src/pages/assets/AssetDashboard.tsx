30e37d336c475f0e3998f9fab916e74df9500804
Jeleel Kolapo <kolly4weall@gmail.com>
2026-08-13 09:46:38 +0100

    open capitalize button

--- client/src/pages/assets/AssetDashboard.tsx
+++ client/src/pages/assets/AssetDashboard.tsx
@@ -81,15 +81,15 @@ const AssetDashboard = () => {
           </p>
         </div>
         <div className="flex space-x-2">
-          {canManageAssets && (
-            <button
-              onClick={() => setShowRecapitalizeModal(true)}
-              className="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              <Calculator className="h-4 w-4 mr-2" />
-              Recapitalize Asset
-            </button>
-          )}
+          {/* {canManageAssets && ( */}
+          <button
+            onClick={() => setShowRecapitalizeModal(true)}
+            className="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+          >
+            <Calculator className="h-4 w-4 mr-2" />
+            Recapitalize Asset
+          </button>
+          {/* )} */}
           <button
             onClick={() => setShowCapitalizeModal(true)}
             className="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
